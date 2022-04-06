f231d05e1a6b8ba0a8ceb2fc0bc803bbfd727099
sapphi-red <green@sapphi.red>
2022-04-06 16:34:00 +0900

    Chore: add missing node prefix

--- src/utils.ts
+++ src/utils.ts
@@ -1,5 +1,5 @@
 import fastglob from 'fast-glob'
-import path from 'path'
+import path from 'node:path'
 import fs from 'fs-extra'
 import pc from 'picocolors'
 import type { Target } from './options'
