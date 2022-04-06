9526e90f4695149eaec8e7c880f8e42ccd6e8719
sapphi-red <green@sapphi.red>
2022-04-06 16:33:08 +0900

    Fix: support absolute-path config.build.outDir
    refs #8

--- src/utils.ts
+++ src/utils.ts
@@ -48,7 +48,8 @@ export const copyAll = async (
   const copyTargets = await collectCopyTargets(rootSrc, targets, flatten)
   await Promise.all(
     copyTargets.map(({ src, dest }) =>
-      fs.copy(path.join(rootSrc, src), path.join(rootSrc, rootDest, dest))
+      // use `path.resolve` because rootDest maybe absolute path
+      fs.copy(path.resolve(rootSrc, src), path.resolve(rootSrc, rootDest, dest))
     )
   )
   return copyTargets.length
