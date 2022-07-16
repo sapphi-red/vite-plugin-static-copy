40778b0deb1f8ad55f3deb7d2c18ed7e37b307a8
sapphi-red <green@sapphi.red>
2022-07-16 23:08:25 +0900

    feat!: bump vite to 3.0.0
    bump supported node version

--- tsup.config.ts
+++ tsup.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'tsup'
 
 export default defineConfig({
   entry: ['src/index.ts'],
-  target: 'node12.2',
+  target: 'node14.18',
   dts: true,
   format: ['esm', 'cjs']
 })
