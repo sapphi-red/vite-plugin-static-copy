d2988123ccc1dc5fb5b24f8b62c04acb296ff1aa
Jisay <jean-christophe.alleman@corp.ovh.com>
2022-10-24 12:03:25 +0200

    test: add rename tests

--- test/fixtures/vite.other.config.ts
+++ test/fixtures/vite.other.config.ts
@@ -8,6 +8,18 @@ export default defineConfig({
         {
           src: 'foo.txt',
           dest: 'fixture1'
+        },
+        {
+          src: 'foo.txt',
+          dest: 'fixture2',
+          rename: 'foo2.txt'
+        },
+        {
+          src: 'foo.txt',
+          dest: 'fixture3',
+          rename: (fileName, fileExtension) => {
+            return `/v1/${fileName}.${fileExtension}`
+          }
         }
       ]
     })
