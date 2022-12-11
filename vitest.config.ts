81c59b9aa6a608b35235983430b14e5f7a4d4156
sapphi-red <green@sapphi.red>
2022-12-11 15:05:53 +0900

    chore: fix type

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,6 +1,4 @@
-/// <reference types="vitest/config" />
-
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vitest/config'
 import GithubActionsReporter from 'vitest-github-actions-reporter'
 
 export default defineConfig({
