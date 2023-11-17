09082e18d24c958d76518d0031873e061dddd848
sapphi-red <49056869+sapphi-red@users.noreply.github.com>
2023-11-17 20:08:35 +0900

    refactor!: drop node 14.18/16/17/19 support

--- tsup.config.ts
+++ tsup.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'tsup'
 
 export default defineConfig({
   entry: ['src/index.ts'],
-  target: 'node14.18',
+  target: 'node18',
   dts: true,
   format: ['esm', 'cjs']
 })
