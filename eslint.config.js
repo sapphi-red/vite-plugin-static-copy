057c16abe0c5eb0c97e3c5dcc2522bd4ed00a4ba
翠 / green <green@sapphi.red>
2024-07-02 20:02:22 +0900

    chore: use flat config (#111)

---
+++ eslint.config.js
@@ -0,0 +1,35 @@
+// @ts-check
+
+import eslint from '@eslint/js'
+import tseslint from 'typescript-eslint'
+import vitest from 'eslint-plugin-vitest'
+import eslintConfigPrettier from 'eslint-config-prettier'
+
+export default tseslint.config(
+  {
+    languageOptions: {
+      sourceType: 'module'
+    },
+    linterOptions: {
+      reportUnusedDisableDirectives: 'error'
+    }
+  },
+  eslint.configs.recommended,
+  ...tseslint.configs.recommended,
+  ...tseslint.configs.stylistic,
+  {
+    rules: {
+      '@typescript-eslint/member-delimiter-style': 'off',
+      '@typescript-eslint/consistent-type-imports': 'error',
+      '@typescript-eslint/consistent-type-definitions': ['error', 'type']
+    }
+  },
+  {
+    files: ['test/**'],
+    ...vitest.configs.recommended
+  },
+  eslintConfigPrettier,
+  {
+    ignores: ['dist/**', 'test/fixtures/**']
+  }
+)
