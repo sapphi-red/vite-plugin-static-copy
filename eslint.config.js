19d9e75b429f9cbafd01f5cdc1b0d4add04efd9b
sapphi-red <49056869+sapphi-red@users.noreply.github.com>
2024-10-09 13:25:51 +0900

    chore: update packages

--- eslint.config.js
+++ eslint.config.js
@@ -2,7 +2,7 @@
 
 import eslint from '@eslint/js'
 import tseslint from 'typescript-eslint'
-import vitest from 'eslint-plugin-vitest'
+import vitest from '@vitest/eslint-plugin'
 import eslintConfigPrettier from 'eslint-config-prettier'
 
 export default tseslint.config(
