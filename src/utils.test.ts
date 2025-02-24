966f3434dfb33837eb990ff70b9be1412036418c
sapphi-red <49056869+sapphi-red@users.noreply.github.com>
2025-02-24 15:30:07 +0900

    test: add unit test for groupTargetsByDirectoryTree

---
+++ src/utils.test.ts
@@ -0,0 +1,32 @@
+import { describe, expect, test } from 'vitest'
+import { groupTargetsByDirectoryTree } from './utils'
+import path from 'node:path'
+
+describe('groupTargetsByDirectoryTree', () => {
+  const defineCase = (input: string[], expected: string[][]) => ({
+    name: input.join(', '),
+    input: input.map(s => ({ resolvedDest: path.resolve(s) })),
+    expected: expected.map(s =>
+      s.map(s => expect.objectContaining({ resolvedDest: path.resolve(s) }))
+    )
+  })
+
+  const cases = [
+    defineCase(['a/b/c'], [['a/b/c']]),
+    defineCase(['a/b/c', 'b/c'], [['a/b/c'], ['b/c']]),
+    defineCase(['a', 'a/b/c'], [['a', 'a/b/c']]),
+    defineCase(['a/b', 'a/b/c'], [['a/b', 'a/b/c']]),
+    defineCase(['a/b/c', 'a/b'], [['a/b/c', 'a/b']]),
+    defineCase(['a', 'a/b/d'], [['a', 'a/b/d']])
+  ] satisfies Array<{
+    name: string
+    input: Array<{ resolvedDest: string }>
+    expected: Array<{ resolvedDest: string }>[]
+  }>
+
+  for (const { name, input, expected } of cases) {
+    test(`groupTargetsByDirectoryTree(${name})`, () => {
+      expect(groupTargetsByDirectoryTree(input)).toStrictEqual(expected)
+    })
+  }
+})
